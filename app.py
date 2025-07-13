5b52cf564c73186a3a6a237db19bafcfb479cc55
christina <christina.ddding@gmail.com>
2025-07-13 22:30:12 +0800

    second commit

--- app.py
+++ app.py
@@ -19,24 +19,27 @@ model = load_model()
 def index():
     return render_template('index.html')
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
 
     try:
-        user_info = {}
-        user_info['age'] = request.form['age']
-        user_info['gender'] = request.form['gender']
-        user_info['height'] = request.form['height']
-        user_info['weight'] = request.form['weight']
-        user_info['workout_type'] = request.form['workout_type']
-        user_info['duration'] = request.form['duration']
-        if user_info['duration'] == 0:
-            return render_template('result.html', predicted_calories=0)
-        user_info['heart_rate'] = request.form['heart_rate']
+        user_data = {}
+        user_data['age'] = request.form['age']
+        user_data['gender'] = request.form['gender']
+        user_data['height'] = request.form['height']
+        user_data['weight'] = request.form['weight']
+        user_data['workout_type'] = request.form['workout_type']
+        user_data['duration'] = request.form['duration']
 
-        predicted_calories = model.predict_user_calories(user_info)
-        return render_template('result.html', predicted_calories=predicted_calories)
+        if user_data['duration'] == 0:
+            return render_template('result.html', predicted_calories=0)
+        user_data['heart_rate'] = request.form['heart_rate']
+
+        predicted_calories = model.predict_user_calories(user_data)
+        return render_template('result.html', prediction=predicted_calories, user_info=user_data)
 
     except Exception as e:
-        return render_template('result.html', predicted_calories=e)
+        return render_template('result.html', prediction=e, user_info=user_data)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
